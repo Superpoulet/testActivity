1a3e1aa0a28d26594f653dcf06ac4a78243de5df
Christian GALLET <cgallet@groupe-uneo.fr>
2019-11-14 17:04:55 +0100

    djyfk

--- metadata.go
+++ metadata.go
@@ -11,6 +11,71 @@ type Input struct {
 	DateDebutRecherche string `md:"dateDebutRecherche,required"`
 	DateFinRecherche   string `md:"dateFinRecherche,required"`
 }
+type CriteresRecherchePaiement struct {
+	DateDebutRecherche string `xml:"dateDebutRecherche,omitempty"`
+
+	DateFinRecherche string `xml:"dateFinRecherche,omitempty"`
+
+	NumeroContrat string `xml:"numeroContrat,omitempty"`
+}
+
+type CodeSystemeExterne string
+
+type LirePaiementPrestationIn struct {
+	CodeSystemeExterne        CodeSystemeExterne        `xml:"codeSystemeExterne"`
+	CriteresRecherchePaiement CriteresRecherchePaiement `xml:"criteresRecherchePaiement,omitempty"`
+}
+
+type SoapBody struct {
+	LirePaiementPrestationIn LirePaiementPrestationIn `xml:"pai:lirePaiementPrestation"`
+}
+
+type SoapEnveloppe struct {
+	XMLName  xml.Name `xml:"soapenv:Envelope"`
+	XmlNS    string   `xml:"xmlns:soapenv,attr"`
+	XmlNS2   string   `xml:"xmlns:pai,attr"`
+	SoapBody SoapBody `xml:"soapenv:Body"`
+}
+
+type SoapEnvelopeOUT struct {
+	XMLName  xml.Name    `xml:"Envelope"`
+	SoapBody SoapBodyOut `xml:"Body"`
+}
+type SoapBodyOut struct {
+	LirePaiementPrestationOut LirePaiementPrestationOut `xml:"lirePaiementPrestationOut"`
+}
+type LirePaiementPrestationOut struct {
+	/*XMLName       xml.Name      `xml:"lirePaiementPrestationOut"`*/
+	ListePaiement ListePaiement `xml:"listePaiement,omitempty"`
+}
+
+type ListePaiement struct {
+	NombreDePaiements int32 `xml:"nombreDePaiements,omitempty"`
+
+	PaiementsParContrat []PaiementsParContrat `xml:"PaiementsParContrat,omitempty"`
+}
+
+type PaiementsParContrat struct {
+	DatePaiement string `xml:"datePaiement,omitempty"`
+
+	Destinataire *Destinataire `xml:"Destinataire,omitempty"`
+
+	LibelleModePaiement string `xml:"libelleModePaiement,omitempty"`
+
+	ModePaiement string `xml:"modePaiement,omitempty"`
+
+	MontantPaiement float32 `xml:"montantPaiement,omitempty"`
+
+	NumeroPaiement int `xml:"numeroPaiement,omitempty"`
+}
+
+type Destinataire struct {
+	Civilite string `xml:"civilite,omitempty"`
+
+	Nom string `xml:"nom,omitempty"`
+
+	Prenom string `xml:"prenom,omitempty"`
+}
 
 func (r *Input) FromMap(values map[string]interface{}) error {
 	strVal, _ := coerce.ToString(values["idContrat"])
